bef9d9e22e358dc7e7b7f512dc2f313b5d3fa8e0
Manuel Mendoza <frontenddesing.manuel@gmail.com>
2021-01-22 19:41:16 -0500

    Login

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <login msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import login from './components/login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    login
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+input{
+  height: 2em;
+}
+
+@import "/asset/css/main.css";
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <form class="form">
+    <h1>Registrarse</h1>
+    <input type="text">
+    <input type="password">
+    <button>Continuar</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+.form{
+  width: 20%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  height: 40vh;
+}
+input{
+  padding: 0px 4%;
+  margin-bottom:4% ;
+}
+
+@media screen and (max-width: 700px) {
+  .form{
+    width: 50%;
+  }
+}
+</style>
